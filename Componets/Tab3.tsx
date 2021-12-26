17d047fac47f7d809d348c2f185aded3571e4074
aref-mehran <aref.mehran.w@gmail.com>
2021-12-26 15:35:35 +0330

    simple player added

--- Componets/Tab3.tsx
+++ Componets/Tab3.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 //import ExploreContainer from '../components/ExploreContainer';
 //import './Tab3.css';
+import ReactAudioPlayer from "react-audio-player";
 
 const Tab3: React.FC = () => {
   return (
@@ -11,6 +12,11 @@ const Tab3: React.FC = () => {
           <IonTitle>Карта города</IonTitle>
         </IonToolbar>
       </IonHeader>
+      <ReactAudioPlayer
+        src="/files/George_Gershwin_playing_Rhapsody_in_Blue.ogg"
+        controls
+      />
+      , salam
     </IonPage>
   );
 };
