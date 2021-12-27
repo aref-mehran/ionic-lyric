b42cdd6a7d59f602bfa8a3e474f3b4c77cf01fbd
aref-mehran <aref.mehran.w@gmail.com>
2021-12-27 11:07:36 +0330

    chakraui added

--- Componets/Tab3.tsx
+++ Componets/Tab3.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ChakraProvider, SimpleGrid } from "@chakra-ui/react";
+
 import { IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 //import ExploreContainer from '../components/ExploreContainer';
 //import './Tab3.css';
@@ -9,14 +11,23 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Карта города</IonTitle>
+          <IonTitle>پخش آهنگ</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <ReactAudioPlayer
-        src="/files/George_Gershwin_playing_Rhapsody_in_Blue.ogg"
-        controls
-      />
-      , salam
+      <ChakraProvider resetCSS>
+        <SimpleGrid columns={2} spacingX={1} spacingY={1}>
+          <ReactAudioPlayer
+            src="/files/George_Gershwin_playing_Rhapsody_in_Blue.ogg"
+            controls
+          />
+        </SimpleGrid>
+        <SimpleGrid mt={10} columns={2} spacingX={1} spacingY={1} height={400}>
+          <div>salam</div>
+          <div>salam</div>
+          <div>salam</div>
+          <div>salam</div>
+        </SimpleGrid>
+      </ChakraProvider>
     </IonPage>
   );
 };
